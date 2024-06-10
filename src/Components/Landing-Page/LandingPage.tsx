d9671f78e9add5055f4537f352947ef44e0c3c0b
Danish Verma <danishverma0003@gmai.com>
2024-06-10 18:22:04 +0530

    Search Functionality Implemented

---
+++ src/Components/Landing-Page/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar"
+import RecipeWigets from "../Recipe-Widgets/RecipeWidgets"
+
+const LandingPage = () => {
+    return(
+        <>
+        <Navbar />
+        <RecipeWigets />
+        </>
+    )
+}
+export default LandingPage
