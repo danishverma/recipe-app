56b38b5a7b40275b5f248dc3e744daafaf00fd51
Danish Verma <danishverma0003@gmai.com>
2024-07-02 11:02:59 +0530

    changed loading css

--- src/Components/Loader/Loading.tsx
+++ src/Components/Loader/Loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
     return (
         <div className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
             <div className="item">
-                <Loader type="spinner-cub" bgColor='' color='red' size={100} />
+                <Loader type="box-rectangular" bgColor='#0033cc' color='' size={150} />
             </div>
         </div>
     );
